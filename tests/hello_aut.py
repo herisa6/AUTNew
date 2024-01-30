feaf7258edf5d35376931cca245824f5592b80e3
herisa6 <64178513+herisa6@users.noreply.github.com>
2024-01-30 10:15:03 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -10,8 +10,23 @@ class AutTest(unittest.TestCase):
         options.add_argument('--ignore-certificate-errors')
         server = 'http://localhost:4444'
 
-        self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
+        # Testing with Firefox
+        self.browser_firefox = webdriver.Remote(command_executor=server, options=options)
+        self.addCleanup(self.browser_firefox.quit)
+
+        # Testing with Chrome
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
+        self.browser_chrome = webdriver.Remote(command_executor=server, options=options)
+        self.addCleanup(self.browser_chrome.quit)
+
+        # Testing with Edge
+        options = webdriver.EdgeOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
+        self.browser_edge = webdriver.Remote(command_executor=server, options=options)
+        self.addCleanup(self.browser_edge.quit)
 
     def test_homepage(self):
         if len(sys.argv) > 1:
@@ -19,12 +34,24 @@ class AutTest(unittest.TestCase):
         else:
             url = "http://localhost"
 
-        self.browser.get(url)
-        self.browser.save_screenshot('screenshot.png')
+        # Testing with Firefox
+        self.browser_firefox.get(url)
+        self.browser_firefox.save_screenshot('screenshot_firefox.png')
         expected_result = "Welcome back, Guest!"      
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
-
+        actual_result = self.browser_firefox.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
 
+        # Testing with Chrome
+        self.browser_chrome.get(url)
+        self.browser_chrome.save_screenshot('screenshot_chrome.png')
+        actual_result_chrome = self.browser_chrome.find_element(By.TAG_NAME, 'p')
+        self.assertIn(expected_result, actual_result_chrome.text)
+
+        # Testing with Edge
+        self.browser_edge.get(url)
+        self.browser_edge.save_screenshot('screenshot_edge.png')
+        actual_result_edge = self.browser_edge.find_element(By.TAG_NAME, 'p')
+        self.assertIn(expected_result, actual_result_edge.text)
+
 if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'],verbosity=2,warnings='ignore')
+    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
