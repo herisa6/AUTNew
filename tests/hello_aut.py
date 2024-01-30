b0ad6aed9a80288b50ebae1a0a3bc87ce39f1f0f
herisa6 <64178513+herisa6@users.noreply.github.com>
2024-01-30 10:17:49 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,6 +1,7 @@
 import unittest, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from concurrent.futures import ThreadPoolExecutor
 
 class AutTest(unittest.TestCase):
 
@@ -10,48 +11,41 @@ class AutTest(unittest.TestCase):
         options.add_argument('--ignore-certificate-errors')
         server = 'http://localhost:4444'
 
-        # Testing with Firefox
+        # Firefox
         self.browser_firefox = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser_firefox.quit)
 
-        # Testing with Chrome
+        # Chrome
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
         self.browser_chrome = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser_chrome.quit)
 
-        # Testing with Edge
+        # Edge
         options = webdriver.EdgeOptions()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
         self.browser_edge = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser_edge.quit)
 
-    def test_homepage(self):
+    def run_test(self, browser, url):
+        browser.get(url)
+        browser.save_screenshot(f'screenshot_{browser.name.lower()}.png')
+        expected_result = "Welcome back, Guest!"
+        actual_result = browser.find_element(By.TAG_NAME, 'p')
+        self.assertIn(expected_result, actual_result.text)
+
+    def test_parallel_execution(self):
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
             url = "http://localhost"
 
-        # Testing with Firefox
-        self.browser_firefox.get(url)
-        self.browser_firefox.save_screenshot('screenshot_firefox.png')
-        expected_result = "Welcome back, Guest!"      
-        actual_result = self.browser_firefox.find_element(By.TAG_NAME, 'p')
-        self.assertIn(expected_result, actual_result.text)
-
-        # Testing with Chrome
-        self.browser_chrome.get(url)
-        self.browser_chrome.save_screenshot('screenshot_chrome.png')
-        actual_result_chrome = self.browser_chrome.find_element(By.TAG_NAME, 'p')
-        self.assertIn(expected_result, actual_result_chrome.text)
-
-        # Testing with Edge
-        self.browser_edge.get(url)
-        self.browser_edge.save_screenshot('screenshot_edge.png')
-        actual_result_edge = self.browser_edge.find_element(By.TAG_NAME, 'p')
-        self.assertIn(expected_result, actual_result_edge.text)
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            executor.submit(self.run_test, self.browser_firefox, url)
+            executor.submit(self.run_test, self.browser_chrome, url)
+            executor.submit(self.run_test, self.browser_edge, url)
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
